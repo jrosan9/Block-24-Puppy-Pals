1e8ab6e40b58f465e8e4bc156d95241a691bddcc
jrosan9 <jbrosan4@gmail.com>
2024-05-07 15:59:04 -0400

    Justin's commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import "./index.css";
+// import "./path-to-css.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+  // console.log(puppies);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+
+      {featPupId && (
+        <div id={"displayInfo"}>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
